18e7b38328c0623eb1e8e425206835bebd5fe2d9
SUDHARSANAN S R <109327528+sudharsanan123@users.noreply.github.com>
2023-02-06 21:59:24 +0530

    Update Duplicate_in_list.py

--- Duplicate_in_list.py
+++ Duplicate_in_list.py
@@ -1,5 +1,5 @@
 list1=list(map(int,input().split()))
-list2=set(list1) 
+list2=set(list1) #set is a built-in function which remove duplicate values
 if len(list1)==len(list2):
     print("No Duplicate")
 else:
